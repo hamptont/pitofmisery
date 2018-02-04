92b3b810986d84cc7c2e7c58810094886f17f9d2
Hampton Terry <haterry@microsoft.com>
2018-02-04 10:11:45 -0800

    initial commit of pit of misery

---
+++ misery/default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="default.aspx.cs" Inherits="_default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Pit of Misery</title>
+
+     <meta charset='utf-8' />
+
+    <script src="./js/main.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script src="https://www.googletagmanager.com/gtag/js?id=UA-113535969-1"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    </form>
+</body>
+</html>
---
+++ misery/js/main.js
@@ -0,0 +1,46 @@
+﻿window.onload = onpageload;
+
+var TWO_BEER_EMOJI = "\ud83c\udf7b";
+
+function onpageload()
+{
+    logGoogleAnalytics();
+    dillydilly();
+    redirectToPitOfMisery();
+}
+
+function dillydilly()
+{
+    // Dilly Dilly
+    alert(TWO_BEER_EMOJI + " DILLY DILLY " + TWO_BEER_EMOJI);
+}
+
+function redirectToPitOfMisery()
+{
+    window.location = "https://www.mlb.com/mariners/ballpark";
+}
+
+function logGoogleAnalytics()
+{
+    window.dataLayer = window.dataLayer || [];
+
+    gtag('js', new Date());
+    gtag('config', 'UA-113535969-1');
+}
+
+function gtag()
+{
+    dataLayer.push(arguments);
+}
+
+function findSurrogatePair(point)
+{
+    // List of all emoji: 
+    // http://unicode.org/emoji/charts/full-emoji-list.html#1f37b
+
+    // assumes point > 0xffff
+    var offset = point - 0x10000,
+        lead = 0xd800 + (offset >> 10),
+        trail = 0xdc00 + (offset & 0x3ff);
+    return [lead.toString(16), trail.toString(16)];
+}
